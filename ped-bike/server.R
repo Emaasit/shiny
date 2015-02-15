c02b7bae5c2d8f26f4dd96b59f4eb918409783e7
Emaasit <daniel.emaasit@gmail.com>
2015-02-14 23:08:43 -0800

    updates

--- ped-bike/server.R
+++ ped-bike/server.R
@@ -9,3 +9,26 @@ library(RColorBrewer)
 library(scales)
 library(lattice)
 library(dplyr)
+
+# read the data files
+all_ped_crashes<-readRDS("data/densityPlot/all_ped_crashes.rds")
+all_bike_crashes<-readRDS("data/densityPlot/all_bike_crashes.rds")
+ped_data<-readRDS("data/countModel/ped_data.rds")
+bike_data<-readRDS("data/countModel/bike_data.rds")
+
+# read the R script that creates the density plots
+source("densityPlot.R")
+
+# read the R script that develops the regression model
+source("regModel.R")
+
+# define the server logic required to draw the crash density plots
+shinyServer(
+        function(input, output, session){
+                output$densityPlot<-renderPlot(
+                        {
+                                
+                        }
+                )
+        }
+)
